829535e987b98987637472fbffdcb486a3da7989
Hem Raj Bhattarai <42824326+HemBabu@users.noreply.github.com>
2022-01-11 20:34:16 +0545

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -25,7 +25,7 @@ in the method parameter) of the linked list: method signature is void
 insertAfter(int index)
 7. Delete - Deletes a node from the linked list using the index: method signature
 is voide deleteAfter(int index)
-8. Search – Search through the linked list to identify the first occurrence of node
+8. Search â€“ Search through the linked list to identify the first occurrence of node
 with match data against the target: method signature is int search(String url)
 9. Display - Displays the complete linked list in a forward manner: method
 signature is String display( )
@@ -34,7 +34,6 @@ signature is String display( )
 */
 
 
-// A simple C++ program for traversal of a linked list
 #include <bits/stdc++.h>
 #include<string.h>
 using namespace std;
@@ -45,17 +44,20 @@ public:
 	Node* next;
 };
 
-// This function prints contents of linked list
-// starting from the given node
+void insertFirst(Node** NODE , string newURL );
+void insertLast(Node ** NODE,string newURL);
+void deleteFirst(Node** NODE);
+void deleteLast(Node** NODE);
 void printList(Node* n)
 {
 	while (n != NULL) {
-		cout << n->data << "\n ";
+		cout<<endl;
+		cout << n->data ;
 		n = n->next;
 	}
 }
 
-// Driver code
+
 int main()
 {
 	Node* head = NULL;
@@ -76,10 +78,92 @@ int main()
 	third->data = "https://www.twitter.com/"; // assign data to third node
 	third->next = NULL;
 
+	insertFirst(&head,"https://www.google.com/");
 	printList(head);
+	deleteFirst(&head);
+	cout<<"\nAfter deleting first url in node\n";
+	printList(head);
+	
+	insertLast(&head,"https://www.google.com/");
+	cout<<"\n\nAfter adding at last first url in node\n";
+printList(head);
+cout<<"\n\nAfter Deleting at last All urls in node are \n";
+
+printList(head);
 
 	return 0;
 }
+void insertFirst(Node** HEAD, string newURL){
+	/* 1. allocate node */
+   // struct Node* new_node = (struct node*) malloc(sizeof(struct Node)); 
+Node* new_node=new Node();
+    /* 2. put in the data */
+    new_node->data = newURL; 
 
-// This is code is contributed by rathbhupendra
+    /* 3. Make next of new node as head */
+    new_node->next = (*HEAD); 
 
+    /* 4. move the head to point to the new node */
+    (*HEAD) = new_node; 
+}
+void deleteFirst(Node** HEAD){
+	 if((*HEAD )== NULL) {
+        cout<<"\nCan not be deleted due to insufficient data on the node\n";
+    }
+
+    
+	Node *tmp=new Node();
+	
+	tmp->next = (*HEAD)->next;
+    delete *HEAD;
+    *HEAD = tmp->next;
+    
+}
+
+void insertLast(Node** END,string newURL){
+	
+    // 1. allocate node 
+    Node* new_node = new Node(); 
+  
+    // Used in step 5 
+    Node *last = *END; 
+  
+    // 2. Put in the data 
+    new_node->data = newURL;  
+  
+    // 3. This new node is going to be  
+    // the last node, so make next of  
+    // it as NULL
+    new_node->next = NULL;  
+  
+    // 4. If the Linked List is empty, 
+    // then make the new node as head 
+    if (*END == NULL)  
+    {  
+        *END = new_node;  
+        return;  
+    }  
+  
+    // 5. Else traverse till the last node 
+    while (last->next != NULL)
+    {
+        last = last->next;  
+    }
+  
+    // 6. Change the next of last node 
+    last->next = new_node;  
+    return; 
+}
+
+void deleteLast(Node** END){
+	if((*END )== NULL) {
+        cout<<"\nCan not be deleted due to insufficient data on the node\n";
+    }
+
+    
+	Node *tmp=new Node();
+	
+	tmp->next = (*END)->next;
+    delete *END;
+    *END = tmp->next;
+}
